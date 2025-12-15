7aaaaf42343c02b7fc15095bc9f8879227985a4f
Angela Mundaca <ange.mundaca@gmail.com>
2025-12-15 01:31:33 -0300

    Comentarios app.js

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+// Proceder para implenetacion de app.js para sprint 2
+
+// Se capturará el texto ingresado en el buscador al enviar el formulario, evitando la recarga de la página por defecto.
+
+// Se realizará una búsqueda de recetas usando una API externa, según el ingrediente ingresado por el usuario.
+
+// Los resultados se mostrarán dinámicamente en pantalla, generando tarjetas HTML con el mismo diseño del Sprint 1.
+
+// Antes de mostrar nuevos resultados, se limpiará el contenedor de recetas.
+
+// Si no se encuentran resultados, se mostrará un mensaje informativo al usuario.
